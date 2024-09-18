c2125f175253bf865e63c7356e94763fe8817dac
Ethan <eethansur@gmail.com>
2024-09-18 17:19:41 -0600

    HW2 First compiled version

---
+++ HW2/.hello.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/osc/OperatingSystems/HW2/hello.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/osc/OperatingSystems/HW2/hello.ko /home/osc/OperatingSystems/HW2/hello.o /home/osc/OperatingSystems/HW2/hello.mod.o
---
+++ HW2/README.md
@@ -0,0 +1,3 @@
+sudo insmod hello.ko
+cat /proc/ethan_maze
+sudo rmmod hello
