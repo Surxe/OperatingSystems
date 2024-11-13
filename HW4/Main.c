56830fe70f418fd1b3068e9210285e98e00f2ae3
Ethan <eethansur@gmail.com>
2024-11-12 18:26:19 -0700

    HW4 fixed print_tasks()

--- HW4/Main.c
+++ HW4/Main.c
@@ -6,9 +6,9 @@ typedef struct {
 	int burst_duration;
 } Task;
 
-void print_tasks(Task tasks[]) {
+void print_tasks(Task tasks[], int num_tasks) {
 	printf("Tasks:\n");
-	for (int i=0; i < sizeof(tasks)/sizeof(tasks[0]); i++) {
+	for (int i=0; i < num_tasks; i++) {
 		printf("Task Name: %s, Arrival Time: %d, Burst Duration %d\n",
 			tasks[i].name, tasks[i].arrival_time, tasks[i].burst_duration);
 	}
@@ -27,7 +27,7 @@ int main() {
 	printf("Num tasks: %d\n", len);
 	
 
-	print_tasks(input);
+	print_tasks(input, len);
 
 	return 0;
 }
