f889648811d462635da04bf9b90f21608830169f
Ethan <eethansur@gmail.com>
2024-11-12 18:59:04 -0700

    HW4 print_schedule initial

--- HW4/Main.c
+++ HW4/Main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 typedef struct {
 	const char *name;
@@ -11,6 +12,28 @@ void print_tasks(Task tasks[], int num_tasks) {
 	for (int i=0; i < num_tasks; i++) {
 		printf("Task Name: %s, Arrival Time: %d, Burst Duration %d\n",
 			tasks[i].name, tasks[i].arrival_time, tasks[i].burst_duration);
+	}
+}
+
+void print_schedule(const char *schedule[], int len) {
+	//Iterate scheduled task names
+	for (int i=0; i< len; i++) {
+		const char *task = schedule[i];
+		char task_string[1] = "I"; //A task's string consisting of symbols # and _
+		
+		//Iterate schedule task names again
+		//Append a symbol at a time to the task string for each time its in the task
+		for (int j=0; j < len; j++) {
+			
+			if (1==1) {
+				//Append the symbol
+				int str_len = strlen(task_string);
+				task_string[str_len] = 'A';
+				task_string[len+1] = '\0';
+			}
+		}
+
+		printf("%s: %s\n", schedule[i], task_string);
 	}
 }
 
@@ -28,6 +51,16 @@ int main() {
 	
 
 	print_tasks(input, len);
+
+	const char *schedule[] = {
+		"Task1Name",
+		"Task2Name",
+		"Task3Name",
+	};
+
+	int len_schedule = sizeof(schedule) / sizeof(schedule[0]);
+
+	print_schedule(schedule, len_schedule);
 
 	return 0;
 }
