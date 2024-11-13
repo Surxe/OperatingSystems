80650852988c013155978d02574499c0ae6cff1a
Ethan <eethansur@gmail.com>
2024-11-12 18:15:06 -0700

    HW4 Task struct initialized

--- HW4/Main.c
+++ HW4/Main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
+typedef struct {
+	const char *name;
+	int arrival_time;
+	int burst_duration;
+} Task;
+
 int main() {
-	printf("Hello");
+	//Use some input
+	Task input[] = {
+		{"Task1Name", 0, 5},
+		{"Task2Name", 1, 6},
+		{"Task3Name", 2, 7},
+	};
+
+	int len = sizeof(input);	
+	printf("Testing");
+	printf("%d", len);
 	return 0;
 }
